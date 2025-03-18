e75efa262fc9b3386b8e013e994b9179040808f5
debedantas <dantasdeborah5@gmail.com>
2025-03-17 21:38:01 -0300

    add função sortearAmigo

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 let amigos = [];
 
+function limparElemento(elemento) {
+    elemento.innerHTML = "";
+}
+
 function adicionarAmigo() {
     let input = document.getElementById("amigo");
     let nome = input.value;
@@ -9,18 +13,46 @@ function adicionarAmigo() {
         return;
     }
 
+    if (amigos.includes(nome)) {
+        alert(`Nome '${nome}' já está na lista`);
+        input.value = "";
+        return;
+    }
+
     amigos.push(nome);
     input.value = "";
-    criarAmigoLista();
+    atualizarAmigoLista();
 }
 
 function atualizarAmigoLista() {
     let lista = document.getElementById("listaAmigos");
-    lista.innerHTML = "";
+    limparElemento(lista);
 
     for (let i = 0; i < amigos.length; i++) {
         let li = document.createElement("li");
         li.innerHTML = amigos[i];
         lista.appendChild(li);
     }
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("Por favor, insira pelo menos um amigo.");
+        return;
+    }
+
+    let indexAleatorio = numeroAleatorio(0, amigos.length);
+    let resultado = document.getElementById("resultado");
+    limparElemento(resultado);
+    resultado.innerHTML = "O amigo secreto sorteado é: " + amigos[indexAleatorio];
+
+    let lista = document.getElementById("listaAmigos");
+    limparElemento(lista);
+    amigos = [];
+}
+
+function numeroAleatorio(min, max) {
+    min = Math.floor(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
 }
